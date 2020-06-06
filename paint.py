75d6c6a73e1deabe2667f2370e7f15693664a533
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-06 12:34:56 +0530

    Update paint.py

--- paint.py
+++ paint.py
@@ -20,7 +20,20 @@ def findColor(img, myColors):
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
-        cv2.imshow("Mask",mask)
+        cv2.imshow(str(color[0]),mask)
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>500:
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            x, y, w, h = cv2.boundingRect(approx)
+    return x+w//2,y
+
 
 while True:
     success,img=cap.read()
