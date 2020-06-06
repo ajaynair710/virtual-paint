ce565af29505fab72b7c2f69d2a13ed48b430f5d
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-06 12:12:59 +0530

    Update paint.py

--- paint.py
+++ paint.py
@@ -14,9 +14,9 @@ myColors = [[5,107,0,19,255,255],
             [90,48,0,118,255,255]]
 
 
-def findColor(img):
+def findColor(img, myColors):
     imgHSV=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower=np.array([h_min,s_min,v_min])
+    lower=np.array([myColors[0][0:2]])
     upper = np.array([h_max, s_max, v_max])
     mask=cv2.inRange(imgHSV,lower,upper)
 
