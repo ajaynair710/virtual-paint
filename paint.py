4377e044101d5c5689ab170bd660306cf7488c75
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-06 12:18:47 +0530

    Update paint.py

--- paint.py
+++ paint.py
@@ -15,13 +15,16 @@ myColors = [[5,107,0,19,255,255],
 
 
 def findColor(img, myColors):
-    imgHSV=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower=np.array([myColors[0][0:2]])
-    upper = np.array([h_max, s_max, v_max])
-    mask=cv2.inRange(imgHSV,lower,upper)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        cv2.imshow("Mask",mask)
 
 while True:
     success,img=cap.read()
+    findColor(img, myColors)
     cv2.imshow("Result",img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
